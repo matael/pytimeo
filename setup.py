ba68349860fd3f6c599f462750200b13a365401a
Mathieu (matael) Gaborit <mat.gaborit@gmx.com>
2013-10-21 08:58:09 +0200

    License

--- setup.py
+++ setup.py
@@ -5,23 +5,18 @@
 #
 # Copyright © 2013 Mathieu Gaborit (matael) <mathieu@matael.org>
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# Distributed under WTFPL terms
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                    Version 2, December 2004
-#
-# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-#
-# Everyone is permitted to copy and distribute verbatim or modified
-# copies of this license document, and changing it is allowed as long
-# as the name is changed.
-#
-#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#  0. You just DO WHAT THE FUCK YOU WANT TO.
-#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from setuptools import setup
 
@@ -32,7 +27,7 @@ setup(
     author='Mathieu (matael) Gaborit',
     author_email='mathieu@matael.org',
     license='WTFPL',
-    url='http://blog.matael.org/',
+    url='https://github.com/Matael/pytimeo',
     py_modules=['pytimeo'],
     install_requires=[
         'beautifulsoup4',
